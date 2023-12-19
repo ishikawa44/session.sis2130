d2f4a6a700a8ac2c394fa39efedcadd0ca402fd9
Myrzakhan0059 <tragssejik@gmail.com>
2023-12-19 20:39:26 +0600

    1.0 тест.

---
+++ cr_hints.py
@@ -0,0 +1,27 @@
+import sqlite3
+
+# Создание базы данных SQLite
+conn = sqlite3.connect('hints.db')
+cursor = conn.cursor()
+
+# Создание таблицы для хранения записей
+cursor.execute('''CREATE TABLE IF NOT EXISTS hints
+                (record_number INTEGER PRIMARY KEY, generated_password TEXT)''')
+conn.commit()
+conn.close()
+
+#задаем первую строку в нашем бд для проверки
+conn = sqlite3.connect('hints.db') #Устанавливает соединение с базой данных SQLite с именем файла "hints.db".
+cursor = conn.cursor() #Создает курсор для выполнения операций с базой данных.
+cursor.execute("SELECT * FROM hints WHERE record_number = 1") #Выполняет SQL-запрос для выбора записи с номером 1 из таблицы "hints".
+record = cursor.fetchone() #Получает результат запроса и сохраняет его в переменную record. Если запись не найдена, record будет пустым.
+if not record:
+    cursor.execute("INSERT INTO hints (record_number, generated_password) VALUES (1, 'Пароль не задан')") #Вставляет новую запись в таблицу "hints" с номером 1 и значением "Пароль не задан".
+    conn.commit() #Фиксирует изменения в базе данных (коммитит транзакцию).
+conn.close() #Закрывает соединение с базой данных SQLite "hints.db".
+
+
+
+
+
+
